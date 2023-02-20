af9648f3118e5679edcebb289a0eb4c2fa129ef3
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2023-02-20 15:15:28 +0100

    Clase y cambios para almacenar en memoria.

--- src/com/iesam/huellas/data/local/registro/RegistroFileLocalDataSource.java
+++ src/com/iesam/huellas/data/local/registro/RegistroFileLocalDataSource.java
@@ -1,7 +1,8 @@
-package com.iesam.huellas.data.local;
+package com.iesam.huellas.data.local.registro;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.iesam.huellas.domain.models.Cat;
 import com.iesam.huellas.domain.models.RegistroAdopcion;
 
 import java.io.File;
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class RegistroFileLocalDataSource {
+public class RegistroFileLocalDataSource implements RegistroLocalRepository{
     private static RegistroFileLocalDataSource instance = null;
     private RegistroFileLocalDataSource (){};
 
@@ -24,7 +25,7 @@ public class RegistroFileLocalDataSource {
 
     private final Type typeAlumnList = new TypeToken<ArrayList<RegistroAdopcion>>() {
     }.getType();
-
+    @Override
     public void save(RegistroAdopcion registroAdopcion) {
         List<RegistroAdopcion> registros = findAll();
         registros.add(registroAdopcion);
@@ -43,7 +44,7 @@ public class RegistroFileLocalDataSource {
         }
     }
 
-
+    @Override
     public RegistroAdopcion findById(String registroId) {
         List<RegistroAdopcion> registros = findAll();
         for (RegistroAdopcion registro : registros) {
@@ -53,7 +54,7 @@ public class RegistroFileLocalDataSource {
         }
         return null;
     }
-
+    @Override
     public List<RegistroAdopcion> findAll() {
         try {
             File myObj = new File(nameFile);
@@ -69,6 +70,18 @@ public class RegistroFileLocalDataSource {
             e.printStackTrace();
         }
         return new ArrayList<RegistroAdopcion>();
+    }
+
+    @Override
+    public void delete(String id) {
+        List<RegistroAdopcion> registroAdopcions = findAll();
+        List<RegistroAdopcion> newList = new ArrayList<>();
+        for (RegistroAdopcion registroAdopcion : registroAdopcions) {
+            if (!Objects.equals(registroAdopcion.getId(), id)) {
+                newList.add(registroAdopcion);
+            }
+        }
+        saveToFile(newList);
     }
 
     public static RegistroFileLocalDataSource getIntance(){
